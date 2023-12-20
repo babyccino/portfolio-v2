219d5072b52b93a24c84174e90d8a027e9c93589
Gus <gus.ryan163@gmail.com>
2023-12-19 19:18:30 -0800

    fix: fix prettier issues

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ["prettier-plugin-astro"],
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  printWidth: 100,
+  arrowParens: "avoid",
+  trailingComma: "all",
+}
+export default config
