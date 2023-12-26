1b63e02ef68fe225e56d3589e7bc4002710dfd90
Gus <gus.ryan163@gmail.com>
2023-12-25 18:32:35 -0800

    style: move experience, etc. to separate pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@
 import { z, defineCollection } from "astro:content"
 // Define a `type` and `schema` for each collection
 
-const schema = z.object({
+const projectsSchema = z.object({
   title: z.string(),
   description: z.string(),
   mainImage: z.object({
@@ -24,10 +24,40 @@ const schema = z.object({
 })
 const projectsCollection = defineCollection({
   type: "content",
-  schema,
+  schema: projectsSchema,
 })
+
+const expierenceSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  started: z.coerce.date(),
+  mainImage: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  mainVideo: z.object({
+    url: z.string(),
+    alt: z.string(),
+  }),
+  otherImages: z.array(
+    z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  ),
+  technologies: z.array(z.string()),
+  links: z.array(z.object({ link: z.string(), icon: z.string() })),
+  content: z.string(),
+})
+const expierenceCollection = defineCollection({
+  type: "content",
+  schema: expierenceSchema,
+})
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   projects: projectsCollection,
+  expierence: expierenceCollection,
 }
-export type ProjectEntry = z.infer<typeof schema>
+export type ProjectEntry = z.infer<typeof projectsSchema>
+export type ExperienceEntry = z.infer<typeof expierenceSchema>
