9a6acc4d15bd9390519ec45c7808f10e6a7a8630
Gus <gus.ryan163@gmail.com>
2024-01-11 02:10:10 -0800

    feat(seo): add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import glsl from "vite-plugin-glsl";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import glsl from "vite-plugin-glsl"
+import icon from "astro-icon"
 
-import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://www.gusryan.me",
+  integrations: [tailwind(), icon(), sitemap()],
   prefetch: true,
   vite: {
-    plugins: [glsl()]
-  }
-});
+    plugins: [glsl()],
+  },
+})
